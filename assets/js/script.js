9a6f5fa1d2307941b34e7478910d476ac12ddce7
Elizabeth Brandt <ebrandt8@uw.edu>
2020-10-27 19:55:41 -0700

    some pseudo code for JS

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+/*
+
+# Tasks
+​
+* setInterval to do the `timed` functionality
+​
+* clearInterval to stop the timeout
+​
+* VAR currentScore/timeLeft
+​
+* VAR questions - Array
+​
+* VAR pointer/index - Current position in the question array
+​
+* VAR startButton
+
+*/
+
+var questions = [
+    {
+        // Question text
+        // List of question answers
+        // Correct answer
+    }
+]
+
+​
+// GIVEN I am taking a code quiz
+​
+// WHEN I click the `start button`
+
+function startGame(){
+​
+}
+
+// 'startButton'.addEventListener("click", startGame);
+​
+// THEN a timer starts and I am presented with a question
+​
+// * Set starting score = 75 (-- to subtract the timer by 1)
+​
+// * Start the interval to begin the score countdown
+​
+
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+​
+/*
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+*/
