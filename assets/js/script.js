254f404fe80e0c3301d5f0a95f555395b4f1914d
Elizabeth Brandt <ebrandt8@uw.edu>
2020-11-10 18:10:45 -0800

    changed variable names and started over on a lot of things

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,12 @@ var shuffledQuestions
 
 // Question and answer variables
 var questionEl = document.getElementById('question');
-var answerButtonsEl = document.getElementById('answer-btns')
+
+var answerButtonsEl = document.getElementById('answer-btns');
+var answer1 = document.getElementById('1');
+var answer2 = document.getElementById('2');
+var answer3 = document.getElementById('3');
+var answer4 = document.getElementById('4');
 
 // VAR startButton
 var startPage = document.getElementById('start-page');
@@ -22,12 +27,12 @@ var startButton = document.getElementById('start-btn');
 var questionContainerEl = document.getElementById('questionContainer')
 
 // OBJECT: QUESTIONS
-var questions = [
+var questionList = [
     {
         // Question text
-        question: 'What is used to repeat an action until a specified condition evaluates to false?',
+        questionText: 'What is used to repeat an action until a specified condition evaluates to false?',
         // List of answers
-        answers: [
+        answerOptions: [
             { text: 'for loop', correct: true },
             { text: 'conditionals', correct: false },
             { text: 'repeat', correct: false },
@@ -35,8 +40,8 @@ var questions = [
         ]
     },
     {
-        question: 'How do you declare a variable?',
-        answers: [
+        questionText: 'How do you declare a variable?',
+        answerOptions: [
             { text: 'vari', correct: false },
             { text: 'var', correct: true },
             { text: 'variable', correct: false },
@@ -44,8 +49,8 @@ var questions = [
         ]
     },
     {
-        question: 'Values that are true or false are known as:',
-        answers: [
+        questionText: 'Values that are true or false are known as:',
+        answerOptions: [
             { text: 'strings', correct: false },
             { text: 'objects', correct: false },
             { text: 'booleans', correct: true },
@@ -53,8 +58,8 @@ var questions = [
         ]
     },
     {
-        question: 'How can you exit a loop?',
-        answers: [
+        questionText: 'How can you exit a loop?',
+        answerOptions: [
             { text: 'exit', correct: false },
             { text: 'return', correct: true },
             { text: 'leave', correct: false },
@@ -62,8 +67,8 @@ var questions = [
         ]
     },
     {
-        question: 'What is the comparison operator for “not equal”?',
-        answers: [
+        questionText: 'What is the comparison operator for “not equal”?',
+        answerOptions: [
             { text: '===', correct: false },
             { text: '<==', correct: false },
             { text: '>==', correct: false },
@@ -71,8 +76,8 @@ var questions = [
         ]
     },
     {
-        question: 'Arrays should be wrapped in:',
-        answers: [
+        questionText: 'Arrays should be wrapped in:',
+        answerOptions: [
             { text: 'curly brackets', correct: false },
             { text: 'parentheses', correct: false },
             { text: 'square brackets', correct: true },
@@ -80,8 +85,8 @@ var questions = [
         ]
     },
     {
-        question: 'The process of outlining your code before you build it is known as:',
-        answers: [
+        questionText: 'The process of outlining your code before you build it is known as:',
+        answerOptions: [
             { text: 'pre-coding', correct: false },
             { text: 'comment-coding', correct: false },
             { text: 'pseudo-coding', correct: true },
@@ -89,8 +94,8 @@ var questions = [
         ]
     },
     {
-        question: 'What is one way to get a user to input information?',
-        answers: [
+        questionText: 'What is one way to get a user to input information?',
+        answerOptions: [
             { text: 'prompt', correct: true },
             { text: 'alert', correct: false },
             { text: 'confirm', correct: false },
@@ -99,132 +104,124 @@ var questions = [
     }
 ]
 
-// GIVEN I am taking a code quiz
+// console.log(questionList);
+// [1].questionText
+// console.log(questionList[1].questionText);
+// console.log(questionList[1].answerOptions[1].correct);
 
+// PRESENT the question = Create new HTML elements for the question content
+
+showQuestion();
+function showQuestion() {
+
+    // SET my current index to 0
+    currentQuestionIndex = 0;
+    // > Set the #questionContainer divs innerHTML = "";
+
+    // for (var i = 0; i > questionList.length; i++) {
+    //     questionEl.textContent = questionList[i].questionText;
+    //     answerButtonsEl.textContent = questionList[i].answerOptions;
+    // }
+    questionEl.textContent = questionList[currentQuestionIndex].questionText
+    // answerButtonsEl.textContent = questionList[currentQuestionIndex].answerOptions;
+    // answer1.textContent = questionList[currentQuestionIndex].answerOptions[0]
+    // answer2.textContent = questionList[currentQuestionIndex].answerOptions[1]
+    // answer3.textContent = questionList[currentQuestionIndex].answerOptions[2]
+    // answer4.textContent = questionList[currentQuestionIndex].answerOptions[3]
+
+
+    // > Append an `h2` for the question text
+
+    // > Append a new `button` for each choice
+
+    var newbutton = document.createElement('button');
+    // button.textContent = questionText
+    // OR
+    // var clickButtonContent = event.target.getAttribute("data-choice", questionText);
+
+    // > The question is correct with clickButtonValue == questions[pointer].answer
+
+    // Change the text of the question to match the randomly selected question from `questionList`
+
+    // Change the text of the corresponding `answerOptions`
+
+    // > button.setAttribute("data-choice", questionText)
+
+
+}
 
 // WHEN I click the `start button`
 startButton.addEventListener('click', startGame)
 
 // THEN the `startQuiz` function runs
-function startGame(){ 
+function startGame() {
     console.log('Started')
     // HIDE the start page
     startPage.classList.add('hide');
-    // SHUFFLE my questions
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    // SET my current index to 0
-    currentQuestionIndex = 0;
     // UNHIDE my question
     questionContainerEl.classList.remove('hide');
     // move to the next question in the array
-    setNextQuestion();
+    showQuestion();
 }
 
 function setNextQuestion() {
     resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+    showQuestion(currentQuestionIndex);
 }
 
-function showQuestion(questions) {
-    questionEl.innerText = questions.question;
-    questions.answers.forEach(answer => {
-        var button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsEl.appendChild(button);
-    })
-}
+
 
 // THEN a timer starts and I am presented with a question
 
-    // * Set starting score = 100 (-- to subtract the timer by 1)
-    var score = 100;
-    // * Start the interval to begin the score countdown
+// * Set starting score = 100 (-- to subtract the timer by 1)
+var score = 100;
+// * Start the interval to begin the score countdown
 
-    // PRESENT the question = Create new HTML elements for the question content
-        // > Set the #questionContainer divs innerHTML = "";
-        // > Append an `h2` for the question text
-        // > Append a new `button` for each choice
 
-        // > var button = document.createElement('button');
-        // > button.textContent = questionText
-        // OR
-        // > button.setAttribute("data-choice", questionText)
+
 
 // WHEN I answer a question
-function resetState() {
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
-    }
-}
 
-function selectAnswer(event) {
-    var selectedButton = event.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-}
-
-function setStatusClass() {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add('correct')
-    } else {
-        element.classList.add('wrong')
-    }
-}
-
-function clearStatusClass() {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
-
-    // * When the user clicks one of the `answerButtons`
+// * When the user clicks one of the `answerButtons`
 
 // WHEN I answer a question incorrectly
-    // setup click eventlistener to listen to all clicks
-    // event.target.matches("button")
-        // > var clickButtonContent = event.target.textContent;
-        // OR
-        // var clickButtonContent = event.target.getAttribute("data-choice", questionText);
+// setup click eventlistener to listen to all clicks
+// event.target.matches("button")
+// > var clickButtonContent = event.target.textContent;
+// OR
+// var clickButtonContent = event.target.getAttribute("data-choice", questionText);
 
-        // > The question is correct with clickButtonValue == questions[pointer].answer
+// > The question is correct with clickButtonValue == questions[pointer].answer
 
 // THEN time is subtracted from the clock
-    // > Subtract points from current score
+// > Subtract points from current score
 
 // THEN I am presented with another question
 
-    // * Increase our pointer by 1
-    // * Clear out the previous question
-    // * Display the next question
+// * Increase our pointer by 1
+// * Clear out the previous question
+// * Display the next question
 
 // WHEN all questions are answered or the timer reaches 0
-    // pointer = questions.length - 1
+// pointer = questions.length - 1
 
 // THEN the game is over
-    // function endGame()
-    // > Transition off of displaying questions
-        // clearInterval(interval)
-        // Clear out current question
-        // Show our results form for getting user's initials
+// function endGame()
+// > Transition off of displaying questions
+// clearInterval(interval)
+// Clear out current question
+// Show our results form for getting user's initials
 
 // WHEN the game is over
 // THEN I can save my initials and score
-    // > ON my initials form "submit"
-        // > var highscore = [];
-        // > GET the currently saved highscores out of localStorage
-            // localStorage.getItem("highscores")
-            // if (storedHighscores), 
-                // then highscores = JSON.parse(storedHighscores)
-                // else highscores = [];
-        // > Create a new list (for highscores)
-        // > Get the value of `initials` input
-        // > Append an object with the initals and highscores
-        // > Store our list of highscores in localStorage (JSON.stringify)
+// > ON my initials form "submit"
+// > var highscore = [];
+// > GET the currently saved highscores out of localStorage
+// localStorage.getItem("highscores")
+// if (storedHighscores), 
+// then highscores = JSON.parse(storedHighscores)
+// else highscores = [];
+// > Create a new list (for highscores)
+// > Get the value of `initials` input
+// > Append an object with the initals and highscores
+// > Store our list of highscores in localStorage (JSON.stringify)
